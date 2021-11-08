1f8b1f4af7ac3271b4d29728adf80b429c882db1
Imoh Precious <jaypee88830@gmail.com>
2021-11-08 01:16:01 +0100

    new commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,10 @@ import React from 'react'
 function TodoItem(props) {
     return (
         <div className="todo">
-            <input type="checkbox" checked={props.item.completed} onChange={()=> console.log("changed")} />
+            <input type="checkbox" 
+                checked={props.item.completed} 
+                onChange={()=> props.handleChange(props.item.id)}
+            />
             <p>{props.item.text}</p>
         </div>
 
