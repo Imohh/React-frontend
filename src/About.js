4f796bc2d61599f261a4a9458dcafbd91c1f45af
Imoh Precious <jaypee88830@gmail.com>
2021-11-11 20:46:15 +0100

    new commit

--- src/About.js
+++ src/About.js
@@ -3,12 +3,31 @@ import {Link} from 'react-router-dom'
 import axios from 'axios'
 
 class About extends React.Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            this.setState({
+                posts: response.data
+            })
+            console.log(response.data)
+        })
+    }
+
     render() {
         return (
             <main style={{ padding: "1rem 0" }}>
               <h2>This is the About Page</h2>
               <Link to="/home">Home</Link> |{" "}
               <Link to="/">Go Back</Link>
+              
             </main>
         )
     }
