c76f87d6d7d3611e6f85726ae7931cdc6f8f8536
Imoh Precious <jaypee88830@gmail.com>
2021-11-14 05:27:10 +0100

    material buttons

--- src/MaterialUi.js
+++ src/MaterialUi.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './App.css'
 import logo from './logo.svg'
+import ButtonGroup from '@material-ui/core/Button'
 import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
 
 function MaterialUi() {
     return (
@@ -9,13 +11,23 @@ function MaterialUi() {
             <h2>Material Ui</h2>
 
             <header className="App-header">
+                <ButtonGroup>
                 <Button 
-                size="large"
-                onClick={()=> alert("hello")} 
-                href="#" 
-                variant="contained" color="secondary">
-                    Hellow World
-                </Button>
+                        startIcon={<SaveIcon />}
+                        size="large"
+                        href="#" 
+                        variant="contained" color="primary">
+                            Save
+                    </Button>
+                    <Button 
+                        startIcon={<SaveIcon />}
+                        size="large"
+                        href="#" 
+                        variant="contained" color="secondary">
+                            Discard
+                    </Button>
+                </ButtonGroup>
+                    
                 <img src={logo} className="App-logo" alt="logo"/>
             </header>
             
