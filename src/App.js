97623404335b2137f19a4176b27ba87f14885409
Imoh Precious <jaypee88830@gmail.com>
2021-11-14 03:03:47 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, Component} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Home from './Home'
 //import Nav from './Nav'
--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,29 @@ import React from 'react'
 import axios from 'axios'
 
 class Home extends React.Component{
+
+    state = {
+        loading: true
+    }
+
     componentDidMount() {
         axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+            this.setState({
+                posts: response.data
+            })
+            console.log(response.data)
+        })
     }
     render() {
+        
         return (
             <div>
                 <h1>This is the Home Page</h1>
                 {/* <Link to="/about">About</Link>
                 <Link to="/">Go Back</Link> */}
     
-    
-    
+                {this.state.loading ? <h1>loading...</h1>:<h1>not loading yet...</h1>}
             </div>
         )
     }
