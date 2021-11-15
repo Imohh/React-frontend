8ddefab00ca24763952bbb1283b645c5ef8c6c46
Imoh Precious <jaypee88830@gmail.com>
2021-11-15 02:15:43 +0100

    material components

--- src/MaterialUi.js
+++ src/MaterialUi.js
@@ -4,26 +4,101 @@ import logo from './logo.svg'
 import ButtonGroup from '@material-ui/core/Button'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import TextField from '@material-ui/core/TextField'
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { orange, green } from '@material-ui/core/colors'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles({
+    root: {
+        background: '#ccc',
+        border: 0,
+        borderRadius: 15,
+        color: 'white',
+        padding: '10px 30px'
+    }
+})
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: orange[500]
+        },
+        secondary: {
+            main: green[500]
+        }
+    }
+})
+
+function ButtonStyled() {
+    const classes = useStyles()
+    return <Button className={classes.root}>Test Styled Button</Button>
+}
+
+
+function CheckboxExample() {
+    const [checked, setChecked] = React.useState(true)
+    return (
+        <div>
+        <FormControlLabel
+            control={<Checkbox
+                checked={checked}
+                icon={<DeleteIcon/>}
+                checkedIcon={<SaveIcon/>}
+                onChange={(e)=>setChecked(e.target.checked)}
+                color="primary"
+                inputProps={{
+                    'aria-label': 'secondary checkbox'
+                }}
+            />}
+            label="Testing Checkbox"
+        />
+            
+        </div>
+    )
+}
 
 function MaterialUi() {
     return (
-        <div>
+        <ThemeProvider theme={theme}>
+
+        <div className="App">
             <h2>Material Ui</h2>
 
             <header className="App-header">
+                <Typography variant="h2">
+                    Welcome to Grep
+                </Typography>
+                <Typography variant="subtitle1">
+                    Learn how to use Material UI
+                </Typography>
+                <ButtonStyled />
+                <TextField
+                    variant="filled"
+                    color="secondary"
+                    type="email"
+                    label="The Time"
+                    placeholder="starboy@gmail.com"
+                />
+                <CheckboxExample/>
                 <ButtonGroup>
-                <Button 
-                        startIcon={<SaveIcon />}
-                        size="large"
-                        href="#" 
-                        variant="contained" color="primary">
-                            Save
-                    </Button>
                     <Button 
                         startIcon={<SaveIcon />}
                         size="large"
                         href="#" 
-                        variant="contained" color="secondary">
+                        variant="contained" 
+                        color="primary">
+                            Save
+                    </Button>
+                    <Button 
+                        startIcon={<DeleteIcon />}
+                        size="large"
+                        href="#" 
+                        variant="contained" 
+                        color="secondary">
                             Discard
                     </Button>
                 </ButtonGroup>
@@ -32,6 +107,7 @@ function MaterialUi() {
             </header>
             
         </div>
+        </ThemeProvider>
     )
 }
 
