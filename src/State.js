0dd6e66d0d841208e3cdbf1dcb463981536e68e5
Imoh Precious <jaypee88830@gmail.com>
2021-11-17 05:04:39 +0100

    new commit

--- src/State.js
+++ src/State.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 export default class State extends React.Component {
+    state = {
+        images: [
+            "https://imohcodes.com/assets/img/header2.jpg",
+            "https://imohcodes.com/assets/img/header2.jpg",
+            "https://imohcodes.com/assets/img/header.jpg",
+        
+        ]
+    }
     render() {
         return(
             <div>
                 State starts here
+                <br/>
+                 <img src={this.state.images[2]} alt="" width="90px"/>
+                
             </div>
         )
     }
