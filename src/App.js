2954bcd0597ca5d4b211b545cc213db5dd9cc371
Imoh Precious <jaypee88830@gmail.com>
2022-01-26 02:06:37 +0100

    scss package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Footer from "./components/Footer";
 
 function App() {
 
+const name = 'Brad'
+
   return (
-    <div>
-      
+    <div className="container">
+
       <h1>This is counter app</h1>
+      <h2>Hello {name}</h2>
+
+      <Footer />
+
 
    </div>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return(
-        <div>
+        <div className="footer-component">
             <h2>Footer</h2>
         </div>
     )
