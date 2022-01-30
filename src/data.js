833df09bce8f46e5c823edc429817fa9dfcda8d6
Imoh Precious <jaypee88830@gmail.com>
2022-01-30 20:37:06 +0100

    new commit

---
+++ src/data.js
@@ -0,0 +1,19 @@
+import image from '../images/one.jpg'
+
+export default [
+    {
+        id: 1,
+    },
+    // {
+    //     id: 2,
+    //     name: 'Ayo Balogun',
+    //     age: 34,
+    //     image: '',
+    // },
+    // {
+    //     id: 1,
+    //     name: 'Travis Scott',
+    //     age: 27,
+    //     image: '',
+    // },
+];
