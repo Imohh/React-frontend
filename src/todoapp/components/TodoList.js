917c6ed3bcbe84ce680c0e379c376cbdf7725ad3
Imoh Precious <jaypee88830@gmail.com>
2022-02-26 00:49:57 +0100

    new commit

--- src/todoapp/components/TodoList.js
+++ src/todoapp/components/TodoList.js
@@ -6,7 +6,7 @@ const TodoList = ({ items, deleteItem, editItem }) => {
             {items.map((item) => (
                 <article className="article" key={item.id}>
                     <p className="">{item.title}</p>
-                    <div>
+                    <div className="buttons-div">
                         <button onClick={() => editItem(item.id)}>Edit</button>
                         <button className="deleteItem" onClick={() => deleteItem(item.id)}>Delete</button>
                     </div>
